bbd1090d8810d138a96700f0d098c4767ace2eb3
Jean-Loup Maillet <jean-loup.maillet@destitech.com>
2023-01-20 22:21:53 +0100

    fix ssh config

--- start.sh
+++ start.sh
@@ -4,11 +4,16 @@ echo "pod started"
 
 if [[ $PUBLIC_KEY ]]
 then
+    sed -i "s/#PubkeyAuthentication.*/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+    sed -i "s/#PasswordAuthentication.*/PasswordAuthentication no/g" /etc/ssh/sshd_config
+    sed -i "s/#AuthorizedKeysFile.*/AuthorizedKeysFile\t\.ssh\/authorized_keys/g" /etc/ssh/sshd_config
+    sed -i "s/#PermitRootLogin.*/PermitRootLogin prohibit-password/g" /etc/ssh/sshd_config
+    sed -i "s/#PermitEmptyPasswords.*/PermitEmptyPasswords no/g" /etc/ssh/sshd_config
+
     mkdir -p ~/.ssh
-    chmod 700 ~/.ssh
     cd ~/.ssh
     echo $PUBLIC_KEY >> authorized_keys
-    chmod 700 -R ~/.ssh
+    chmod 644 -R ~/.ssh
     cd /
     service ssh start
 fi
